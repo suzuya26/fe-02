ae442e78f6ab5d2ae8635148ff024178a95cb4df
Gianmaulana10 <107259559+Gianmaulana10@users.noreply.github.com>
2022-06-15 21:48:13 +0700

    CardList (prog)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/Layouts/NavBar';
+import CardList from './components/Layouts/CardList';
 function App() {
   return (
     <div className="App">
       <header>
         <NavBar/>
       </header>
+      <CardList/>
     </div>
   );
 }
