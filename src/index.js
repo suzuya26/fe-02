70a9c2af25c52eb31e882b905bde293129f2eddd
Farhan Septiadi <55498270+suzuya26@users.noreply.github.com>
2022-06-15 20:50:59 +0700

    nambah yarn router + coba implement route signin

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import SignInSide from './components/SignInSide'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/signin" element ={
+            <SignInSide/>
+          } />
+        </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
