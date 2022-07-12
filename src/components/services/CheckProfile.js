217fed8fff5b2150754951ece2a2d7acdfa72ad5
Farhan Septiadi <55498270+suzuya26@users.noreply.github.com>
2022-07-12 16:04:21 +0700

    berhasil fix reducer tidak kepanggil

--- src/components/services/CheckProfile.js
+++ src/components/services/CheckProfile.js
@@ -1,13 +1,9 @@
 import React, { useEffect } from "react";
 import { Navigate } from "react-router-dom";
-import jwtDecode from 'jwt-decode';
 import { useDispatch, useSelector } from "react-redux";
 import { getCurrentUser } from "../../actions/userAction";
 
 function CheckProfile ({children}){
-    const token = localStorage.getItem("token");
-    console.log(token);
-    const decoded = jwtDecode(token);
     const { listUserResult, listUserLoading, listUserError } = useSelector((state) => state.userReducer);
     const dispatch = useDispatch();
 
@@ -15,18 +11,8 @@ function CheckProfile ({children}){
         console.log("1. use effect component did mount")
         dispatch(getCurrentUser());
     }, [dispatch])
-
-    listUserResult ? (
-        listUserResult.map((user) => {
-            return (
-                console.log("uzah getto")
-            )
-        })
-    ) : listUserLoading ? (
-        console.log("loding cuy")
-    ) : (
-        console.log("eror cuy")
-    )
+    console.log(listUserResult.alamat)
+    if(listUserResult.alamat === null) return <Navigate to='/lengkapi-profil'/>
 
     return children
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import  productReducer from "./product";
-import  userReducer from "./product"
+import  userReducer from "./user"
 
 export default combineReducers({
     productReducer,
